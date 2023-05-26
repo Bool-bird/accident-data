a884b46eb4d0b894e2ab93c4815f3d745641cdc4
Soyeon <annkwon1123@gmail.com>
2023-05-26 13:13:27 +0900

    data

--- util/preprocessing2.py
+++ util/preprocessing2.py
@@ -8,7 +8,7 @@ import pandas as pd
 def extract_yyyymmdd(s):
     date = s.split()[0]
     list =  date.split('-')
-    return list[0]+list[1]+list[2]
+    return int(list[0]+list[1]+list[2])
 
 # 사고신고사유를 분리하는 함수
 def extract_sago(s):
@@ -23,37 +23,12 @@ def extract_sago(s):
         rtn[3] += 1
     return rtn
 
-# 비사고데이터의 "공사비"를 추출하는 함수
-def extract_cost(data_str):
-
-    if '~' in data_str:
-        # 범위형 데이터인 경우
-        # 문자열을 '~' 기준으로 분리하여 최소값과 최대값을 추출합니다.
-        range_list = data_str.split('~')
-        min_value = int(range_list[0])
-        max_value = int(range_list[1][:-1])
-
-        # 최소값과 최대값을 더한 후 2로 나누어 중앙값을 계산합니다.
-        median_value = (min_value + max_value) / (2.0 * 100)
-
-    elif '초과' in data_str:
-        value = int(data_str[:2])
-        # n% 초과일 경우 n.5%로 가정하여 중앙값을 계산합니다.
-        median_value = (value + 100) / (2.0 * 100)
-
-    elif '미만' in data_str:
-        value = int(data_str[:2])
-        # n% 미만일 경우 n-0.5%로 가정하여 중앙값을 계산합니다.
-        median_value = value / 100
-
-    else:
-        # 범위형, 이상형, 미만형 데이터가 아닌 경우
-        # 예외 처리를 하거나 None 값을 반환합니다.
-        median_value = None
-
-    # 중앙값을 반환합니다.
-    return median_value
-
+'''
+'100억초과 200억이하' '20억초과 30억이하' '200억초과 300억이하' '50억초과 60억이하' '40억초과 50억이하'
+ '90억초과 100억이하' '1억초과 5억이하' '10억초과 20억이하' '300억초과 400억이하' '5억초과 10억이하'
+ '80억초과 90억이하' '30억초과 40억이하' '70억초과 80억이하' '500억초과 1000억이하'
+ '400억초과 500억이하' '1000억초과 2000억이하' '60억초과 70억이하' '2000억초과' '1억이하'
+ '''
 
 # 기온의 이상치를 판별하는 함수
 def is_normal_temper(temper):
@@ -77,45 +52,55 @@ params ={'serviceKey' : serviceKey, 'pageNo' : '1', 'numOfRows' : '10', 'dataTyp
 
 # 해당 날짜의 평균 기온를 반환하는 함수
 def get_temper(yyyymmdd):
-    params['startDt'] = yymmdd 
-    params['endDt'] = yymmdd+1
-    response = requests.get(url, params=params)
-    jsondata = json.loads(response.content)
+    params['startDt'] = yyyymmdd 
+    params['endDt'] = yyyymmdd+1
+    try:
+        response = requests.get(url, params=params)
+        jsondata = json.loads(response.content)
 
-    for item in jsondata['response']['body']['items']['item']:
-        return(item['avgTa'])
+        for item in jsondata['response']['body']['items']['item']:
+            return float(item['avgTa'])
+    except ValueError:
+        return 0.0
+    except KeyError:
+        return None
 
 # 해당 날짜의 평균 기온를 반환하는 함수(지역)
-def get_temper(yyyymmdd, loc):
-    params['startDt'] = yymmdd 
-    params['endDt'] = yymmdd+1
+def get_temper2(yyyymmdd, loc):
+    params['startDt'] = yyyymmdd 
+    params['endDt'] = yyyymmdd+1
     params['stnIds'] = loc_code(loc)
     response = requests.get(url, params=params)
     jsondata = json.loads(response.content)
 
     for item in jsondata['response']['body']['items']['item']:
-        return(item['avgTa'])
+        return float(item['avgTa'])
     
 # 해당 날짜의 평균 습도를 반환하는 함수
 def get_humid(yyyymmdd):
-    params['startDt'] = yymmdd 
-    params['endDt'] = yymmdd+1
-    response = requests.get(url, params=params)
-    jsondata = json.loads(response.content)
+    try:
+        params['startDt'] = yyyymmdd 
+        params['endDt'] = yyyymmdd+1
+        response = requests.get(url, params=params)
+        jsondata = json.loads(response.content)
 
-    for item in jsondata['response']['body']['items']['item']:
-        return(item['avgRhm'])  
+        for item in jsondata['response']['body']['items']['item']:
+            return float(item['avgRhm'])  
+    except ValueError:
+        return 0.0
+    except KeyError:
+        return None
 
 # 해당 날짜의 평균 습도를 반환하는 함수
-def get_humid(yyyymmdd, loc):
-    params['startDt'] = yymmdd 
-    params['endDt'] = yymmdd+1
+def get_humid2(yyyymmdd, loc):
+    params['startDt'] = yyyymmdd 
+    params['endDt'] = yyyymmdd+1
     params['stnIds'] = loc_code(loc)
     response = requests.get(url, params=params)
     jsondata = json.loads(response.content)
 
     for item in jsondata['response']['body']['items']['item']:
-        return(item['avgRhm']) 
+        return float(item['avgRhm']) 
 
 # 지역 코드를 반환하는 함수
 def loc_code(loc):
